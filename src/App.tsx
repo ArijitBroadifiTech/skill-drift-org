9ab7b70a1fe365d4407d8a506b1b2fce3eaae277
Arijit Khan <arijit.khan@broadifitech.com>
2025-11-10 16:37:25 +0530

    feat: added shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import './App.css'
+import './App.css';
+import Test from './components/Test';
 
 function App() {
   return (
-    <>
-      <p className="text-red-300 bg-black">Hi there</p>
-    </>
-  )
+    <div className='w-full h-full min-h-screen bg-black'>
+      <p className='text-red-300 bg-black'>Hi there</p>
+      <Test />
+    </div>
+  );
 }
 
-export default App
+export default App;
