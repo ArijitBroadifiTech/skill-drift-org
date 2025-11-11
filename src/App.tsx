197ff20f45ae6091a6caf28434030a4a499c6352
Arijit Khan <arijit.khan@broadifitech.com>
2025-11-11 12:42:18 +0530

    feat: updated pre-commit file

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Button } from '@/components/ui/button';
 
 function App() {
   return (
-    <div className='flex min-h-svh flex-col items-center justify-center'>
-      <Button>Click me</Button>
-      <div className='bg-red-200'>Hi there</div>
+    <div className='flex min-h-svh flex-col items-center justify-center bg-black'>
+      <Button className='bg-white'>Click me</Button>
+      <button className='bg-amber-300 p-2 text-black font-bold'>Again!!</button>
     </div>
   );
 }
